075cb3ed788e3feb008b80dd9763033b9617a034
Kevin Martinez <kevinvr@hotmail.es>
2022-03-08 00:30:01 -0600

    final

--- aco.py
+++ aco.py
@@ -1,19 +1,22 @@
 from turtle import distance
 from graph import Edge, Graph
 import numpy as np
-import random 
+import random
 import functools
+
 
 class Ant:
     def __init__(self) -> None:
         self.route = []
         self.position = None
         self.cost = 0
+
     def __str__(self) -> str:
         return f'Cost:{self.cost:.3f}\tRuta:{[step for step in self.route]}'
 
+
 class ACO:
-    def __init__(self, graph:Graph, alpha:float, beta:float, Q: float, ants: int, disipation: float, initial_point:int, final_point:int, seed = 0) -> None:
+    def __init__(self, graph: Graph, alpha: float, beta: float, Q: float, ants: int, disipation: float, initial_point: int, final_point: int, seed=0) -> None:
         self.graph = graph
         self.alpha = alpha
         self.beta = beta
@@ -27,41 +30,31 @@ class ACO:
         random.seed(seed)
         self.new_gen()
 
-    def get_edge(self, x:int, y:int):
-        point = self.graph.get_node(x)
-        return point.get_edge(y)
-    
-
     def new_gen(self):
         self.ants = [Ant() for i in range(0, self.ants_len)]
         for ant in self.ants:
             ant.position = self.start
-        
-    def __a(self, edge: Edge):
-        # print('a', edge.pheromones ** self.alpha * (1/(0.000001 if edge.distance==0 else edge.distance))**self.beta)
-        distance = 0.000001 if edge.distance == 0 else edge.distance
-        try:
-            # return edge.pheromones ** self.alpha * (1/distance)**self.beta
-            # print(distance)
-            test= (1/distance)**self.beta
-        except RuntimeWarning:
-            print(distance)
-        return 1
 
-    def generate_solutions(self)->None:
+    def __a(self, edge: Edge):
+        distance = 0.000001 if edge.distance == 0 else edge.distance
+        return edge.pheromones ** self.alpha * (1/distance)**self.beta
+
+    def generate_solutions(self) -> None:
         for ant in self.ants:
             while (ant.position != self.end):
                 node = self.graph.points[ant.position]
-                edges = node.edges
-                #sumatoria
-                total_strength = functools.reduce(lambda res, edge: res + self.__a(edge), edges, 0.0)
+                edges = node.edges.copy()
+                edges.sort(key=self.__a)
+                total_strength = functools.reduce(
+                    lambda res, edge: res + self.__a(edge), edges, 0.0)
                 # print('total', total_strength)
+                r = random.uniform(0, 1)
                 for edge in edges:
-                    r = random.uniform(0,1)
                     p = self.__a(edge)/total_strength
                     if r < p:
                         ant.position = edge.index
-                        ant.route.append((node.label, self.graph.points[edge.index].label))
+                        ant.route.append(
+                            (node.label, self.graph.points[edge.index].label))
                         # print(edge.distance)
                         ant.cost += edge.distance
                         break
@@ -73,8 +66,9 @@ class ACO:
                 for ant in self.ants:
                     if (point.label, self.graph.points[edge.index].label) in ant.route:
                         accumulator += self.Q/ant.cost
-                edge.pheromones = (1-self.disipation)*edge.pheromones+ accumulator
-    
+                edge.pheromones = (1-self.disipation) * \
+                    edge.pheromones + accumulator
+
     def __str__(self) -> str:
         res = ''
         return res
--- main.py
+++ main.py
@@ -1,60 +1,152 @@
-from aco import ACO
+from aco import ACO, Ant
+import matplotlib.pyplot as plt
 from graph import Edge, Graph, Point
 import numpy as np
 
-m=10
-domain = (-5.0,5.0)
-increment = 0.5
+m = 10
+domain = (0, np.pi)
+increment = 0.1
 
-# ecuacion general
+# Ecuacion general
 def F(x, i) -> float:
     return -np.sin(x)*np.power(np.sin((i*x**2)/np.pi), 2*m)
 
-# def get_index(list, label)->int:
-#     for item in enumerate(list):
-#         if 
+# Ecuación a dos dimensiones
+def f(x, y): return F(x, 1)+F(y, 2)
 
-def generate_graph()->Graph:
+# Punto a buscar
+# print(f(2.20,1.57))
+
+
+def generate_graph() -> Graph:
+    # Se vuelve discreto el dominio
     discrite_values = []
     i = domain[0]
     while i <= domain[1]:
         discrite_values.append(i)
-        i+= increment
+        i += increment
+    
+    # Se comienza la creación de nodos
     nodes = []
-    nodes.append(Point('Initial',None))
+    # Nodo inicial
+    nodes.append(Point('Initial', None))
     initial = nodes[0]
+    # Nodos de la variable x
     for v in discrite_values:
         nodes.append(Point(f'{v}_x', v))
+
+    # Nodos de la variable y
     for v in discrite_values:
         nodes.append(Point(f'{v}_y', v))
+
+    # Nodo final
     nodes.append(Point('Final', None))
     final_index = len(nodes)-1
 
-    for (i,val)in enumerate(discrite_values):
-        initial.edges.append(Edge(i+1,val,F(val,1),0.01))
+    # Conexión del nodo inicial con x
+    for (i, val) in enumerate(discrite_values):
+        distance = F(val, 1)*-1
+        distance = 0.0000000001 if distance == 0 else distance
+        distance = 1/distance
+        initial.edges.append(Edge(i+1, val, distance, 0.01))
 
+    # Conexión de los nodos x con los nodos y
     second_pahse_index = len(discrite_values)
     for i in range(len(discrite_values)):
         current = nodes[i+1]
-        for (i,val) in enumerate(discrite_values):
-            current.edges.append(Edge(i+second_pahse_index+1,val,F(val,2),0.01))
+        for (i, val) in enumerate(discrite_values):
+            distance = F(val, 2)*-1
+            distance = 0.0000000001 if distance == 0 else distance
+            distance = 1/distance
+            current.edges.append(
+                Edge(i+second_pahse_index+1, val, distance, 0.01))
 
+    # Conexión de los nodos y con el nodo final
     for i in range(len(discrite_values)):
-        nodes[i+second_pahse_index+1].edges.append(Edge(final_index,None,0,0.01))
+        nodes[i+second_pahse_index +
+              1].edges.append(Edge(final_index, None, 0, 0.01))
 
     return Graph(nodes)
 
-
-
+# Inicialización del grafo
 graph = generate_graph()
-aco = ACO(graph=graph, alpha=0.5, beta=0.9, Q= 1, ants=10, disipation= 0.3, initial_point=0, final_point=len(graph.points)-1)
-# for point in graph.points:
-#     print(point)
-# print(F(2,1)+F(-3.0,2))
-for i in range(1):
-    print("gen", i)
+aco = ACO(graph=graph, alpha=0.5, beta=1, Q=0.5, ants=10,
+          disipation=0.01, initial_point=0, final_point=len(graph.points)-1, )
+
+# Ciclo principal
+bests = []
+averages = []
+indexes = []
+percentage = 0
+iteration = 0
+while percentage < 0.6:
+    iteration += 1
+    indexes.append(iteration)
+    # Realiza el recorrido para cada hormiga
     aco.generate_solutions()
+    # Actualiza el nivel de feromonas
     aco.pheromone_update()
+
+    # Datos para las graficas
+    avg = 0
+    min = Ant()
+    min.cost = 100
     for ant in aco.ants:
-        print(ant)
+        if ant.cost < min.cost:
+            min = ant
+        avg += ant.cost
+    avg /= len(aco.ants)
+    bests.append(min)
+    averages.append(avg)
+
+    #Chequeo del porcentaje de hormigas dando el mismo valor
+    percentage = 0
+    for ant in aco.ants:
+        if min.cost == ant.cost:
+            percentage += 1
+    percentage /= len(aco.ants)
+
+    # Nueva generación de hormigas
     aco.new_gen()
+
+best_route = bests[-1].route
+best_x = aco.graph.node_by_label(best_route[1][0]).value
+best_y = aco.graph.node_by_label(best_route[1][1]).value
+best_z = f(best_x, best_y)
+
+# Graficación del proceso
+resolution = 150
+fig = plt.figure(figsize=plt.figaspect(0.4))
+fig.suptitle("Busqueda de solución mediante Colonia de hormigas")
+fig.tight_layout(pad=10)
+
+# Evolucion
+ax = fig.add_subplot(1, 2, 2)
+ax.set_title("Evolución")
+ax.plot(indexes, [x.cost for x in bests], label="Mejores")
+ax.plot(indexes, averages, label="Promedios")
+ax.set_xlabel("Generación")
+ax.set_ylabel("Evaluación")
+ax.legend()
+
+# Grafica de la función
+ax = fig.add_subplot(1, 2, 1, projection='3d')
+ax.set_title("Función de Michalewicz")
+x = np.linspace(0, np.pi, resolution)
+y = np.linspace(0, np.pi, resolution)
+X, Y = np.meshgrid(x, y)
+Z = F(X, 1)+F(Y, 2)
+ax.contourf(X, Y, Z, resolution)
+print(bests[-1])
+ax.scatter(best_x, best_y, best_z, label="Mejor punto encontrado")
+ax.legend()
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.85,
+                    wspace=0.4,
+                    hspace=0.4)
+plt.figtext(0.5, 0.01, f"Mejor punto encontrado x={best_x:.3f} y={best_y:.3f} z={best_z:.3f}", ha="center",
+            fontsize=10,)
+plt.show()
